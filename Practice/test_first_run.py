9d8c685ed7d77daccf8c29b1527e306bb3246e4c
Damodhar <dhamuvallavarapu@gmail.com>
2025-08-29 16:05:44 +0530

    first commit

---
+++ Practice/test_first_run.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+
+def test_lambdatest_playground():
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    driver.get("https://www.lambdatest.com/learning-hub/webdriver")
+    print("Title is : ",driver.title)
+    driver.close()
+    driver.quit()
+
+def test2_lambdatest_playground():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.inforida.com")
+    print("Title is : ",driver.title)
+    driver.close()
+    driver.quit()
+
+def main():
+    test_lambdatest_playground()
+    test2_lambdatest_playground()
+if __name__ == '__main__':
+    main()
